bb17a2affb6883f5cb543763ba434a599deb36c4
Ivanisj <isrvsj@gmail.com>
2025-03-19 14:43:07 +0100

    ver2

--- src/Empleado.java
+++ src/Empleado.java
@@ -6,4 +6,9 @@ public class Empleado {
     private int edad;
     private double sueldo;
 
+    public Empleado(double sueldo, int edad, String nombre) {
+        this.sueldo = sueldo;
+        this.edad = edad;
+        this.nombre = nombre;
+    }
 }
--- src/Empresa.java
+++ src/Empresa.java
@@ -1,10 +1,9 @@
 
 public class Empresa {
     public static void main(String[] args) {
-        Empleado e1= new Empleado();
+        Empleado e1= new Empleado( );
         System.out.println(e1);
         Departamento d1= new Departamento();
-
         d1.anadir(e1);
         System.out.println(d1);
     }
