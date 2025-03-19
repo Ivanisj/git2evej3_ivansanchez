ac3625dea7772b7c5cb458b7426a628c4b6706e3
Ivanisj <isrvsj@gmail.com>
2025-03-19 14:38:46 +0100

    ver1

---
+++ src/Empleado.java
@@ -0,0 +1,9 @@
+
+
+
+public class Empleado {
+    private String nombre;
+    private int edad;
+    private double sueldo;
+
+}
---
+++ src/Empresa.java
@@ -0,0 +1,11 @@
+
+public class Empresa {
+    public static void main(String[] args) {
+        Empleado e1= new Empleado();
+        System.out.println(e1);
+        Departamento d1= new Departamento();
+
+        d1.anadir(e1);
+        System.out.println(d1);
+    }
+}
